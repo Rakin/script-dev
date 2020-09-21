8e2bf996ebb183dbf27f1845961f34cf5d252d7d
Rakin <rafael.kin.78@gmail.com>
2020-09-21 15:51:01 -0300

    Editada variaveis de ambiente laravel

--- create-laravel-api.sh
+++ create-laravel-api.sh
@@ -7,18 +7,10 @@ docker run  \
 -v "$(pwd)"/:/app \
 -u $(id -u ${USER}):$(id -g ${USER}) \
 composer \
-bash -c "echo \$(id -u \${USER}):\$(id -g \${USER}) "
-"&& composer create-project --prefer-dist specialtactics/laravel-api-boilerplate '$projeto';"
+bash -c "echo \$(id -u \${USER}):\$(id -g \${USER}) && composer create-project --prefer-dist specialtactics/laravel-api-boilerplate '$projeto';"
 
-chown -R developer:developer ./
+sed -ri -e 's!DB_CONNECTION=pgsql!DB_CONNECTION=mysql!g' "./$projeto/.env"
+sed -ri -e 's!DB_PORT=5432!DB_PORT=3306!g' "./$projeto/.env"
+sed -ri -e 's!DB_DATABASE=your_project_local!DB_DATABASE='"$projeto"'!g' "./$projeto/.env"
+sed -ri -e 's!DB_USERNAME=laradock!DB_USERNAME=root!g' "./$projeto/.env"
 
-cd "$projeto"
-
-sed -ri -e 's!DB_CONNECTION=pgsql!DB_CONNECTION=mysql!g' ./.env
-sed -ri -e 's!DB_PORT=5432!DB_PORT=3306!g' ./.env
-sed -ri -e 's!DB_DATABASE=your_project_local!DB_DATABASE='"$projeto"'!g' ./.env
-sed -ri -e 's!DB_USERNAME=laradock!DB_USERNAME=root!g' ./.env
-
-git init \
-  && git add . \
-  && git commit -m "First commit"
