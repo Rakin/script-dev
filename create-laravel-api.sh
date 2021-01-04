14b5a1d47027107047a41b6d8fa2ff36e183dbef
Rakin <rafael.kin.78@gmail.com>
2021-01-04 09:16:11 -0300

    Remove edicao do arquivo .env

--- create-laravel-api.sh
+++ create-laravel-api.sh
@@ -8,9 +8,3 @@ docker run  \
 -u $(id -u ${USER}):$(id -g ${USER}) \
 composer \
 bash -c "echo \$(id -u \${USER}):\$(id -g \${USER}) && composer create-project --prefer-dist specialtactics/laravel-api-boilerplate '$projeto';"
-
-sed -ri -e 's!DB_CONNECTION=pgsql!DB_CONNECTION=mysql!g' "./$projeto/.env"
-sed -ri -e 's!DB_PORT=5432!DB_PORT=3306!g' "./$projeto/.env"
-sed -ri -e 's!DB_DATABASE=your_project_local!DB_DATABASE='"$projeto"'!g' "./$projeto/.env"
-sed -ri -e 's!DB_USERNAME=laradock!DB_USERNAME=root!g' "./$projeto/.env"
-
