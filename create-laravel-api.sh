13a8fb39cea0f2894dcf4e379d83e8d653a98770
Rakin <rafael.kin.78@gmail.com>
2021-01-06 15:55:10 -0300

    Add devcontainer

--- create-laravel-api.sh
+++ create-laravel-api.sh
@@ -7,4 +7,18 @@ docker run  \
 -v "$(pwd)"/:/app \
 -u $(id -u ${USER}):$(id -g ${USER}) \
 composer \
-bash -c "echo \$(id -u \${USER}):\$(id -g \${USER}) && composer create-project --prefer-dist specialtactics/laravel-api-boilerplate '$projeto';"
+bash -c "echo \$(id -u \${USER}):\$(id -g \${USER}) && composer create-project --prefer-dist laravel/laravel '$projeto';"
+
+bash -c "echo \$(id -u \${USER}):\$(id -g \${USER}) && composer create-project --prefer-dist laravel/laravel '$projeto';"
+
+cd "$projeto"
+git init
+git add .
+git commit -m "Instalacao laravel"
+
+git submodule add git@github.com:desize/.devcontainer.git
+git init
+git add .
+git commit -m "Instalacao .devcontainer"
+
+code .
